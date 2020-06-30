6931c555c31e83f8e990d6f51028466bd8220102
Asuk Kono <asukiaaa@gmail.com>
2020-06-30 15:34:46 +0900

    Show log on bottom container

--- front/src/views/home.tsx
+++ front/src/views/home.tsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import MonacoEdotor from 'react-monaco-editor'
-import { Button, Grid } from '@material-ui/core'
+import { Box, Button, Grid } from '@material-ui/core'
 import api from '../api'
 import io from 'socket.io-client'
 
 const socket = io('/')
-let logText = ''
 
 export default () => {
   const [mainSrc, setMainSrc] = useState()
@@ -13,37 +12,54 @@ export default () => {
   const [requirementsSrc, setRequirementsSrc] = useState()
   const [requirementsEditor, setRequirementsEditor] = useState<any>()
   const playProgram = async () => {
-    logText = ''
     await api.postMainPy(mainEditor?.getModel().getValue())
     await api.postRequirementsTxt(requirementsEditor?.getModel().getValue())
     await api.run()
   }
   const [connected, setConnected] = useState(false)
-  const [logCount, setLogCount] = useState(0)
-
-  socket.on('connect', () => {
-    setConnected(true)
-  })
-  socket.on('disconnect', () => {
-    setConnected(false)
-  })
-  socket.on('message', (data: any) => {
-    console.log(data)
-    logText += data
-    setLogCount(logCount + 1)
-  })
+  const [logText, setLogText] = useState('')
 
   useEffect(() => {
+    socket.on('connect', () => {
+      setConnected(true)
+    })
+    socket.on('disconnect', () => {
+      setConnected(false)
+    })
+    return () => {
+      socket.removeListener('connect')
+      socket.removeListener('disconnect')
+    }
+  }, [connected])
+  useEffect(() => {
+    socket.on('message', (data: any) => {
+      console.log(data)
+      setLogText(logText + data)
+    })
+    return () => {
+      socket.removeListener('message')
+    }
+  }, [logText])
+
+  useEffect(() => {
+    const windowHeight = window.innerHeight
+    const middleContainerHeight = document.getElementById('middle-container')?.offsetHeight
+    const topContainerHeight =  document.getElementById('top-container')?.offsetHeight
+    const bottomContainer =  document.getElementById('bottom-container')
+    console.log(bottomContainer, middleContainerHeight, topContainerHeight)
+    if (bottomContainer && middleContainerHeight && topContainerHeight) {
+      bottomContainer.style.height = `${windowHeight - topContainerHeight - middleContainerHeight}px`
+    }
     const f = async () => {
       setMainSrc(await api.getMainPy() || '')
       setRequirementsSrc(await api.getRequitementsTxt() || '')
     }
     f()
-  })
+  }, [])
 
   return (
-    <div className="Home" style={{ width: '100vw', height: '100vh' }}>
-      <div style={{ width: '100%', height: '70%', flexFlow: 'row', display: 'flex' }}>
+    <div className="Home" style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
+      <div id="top-container" style={{ width: '100%', height: '70%', flexFlow: 'row', display: 'flex' }}>
         <MonacoEdotor
           width='50%'
           height='100%'
@@ -51,6 +67,9 @@ export default () => {
           value={mainSrc}
           editorDidMount={(editor, monaco) => {
             setMainEditor(editor)
+          }}
+          options={{
+            automaticLayout: true,
           }}
         />
         <MonacoEdotor
@@ -61,15 +80,30 @@ export default () => {
           editorDidMount={(editor, monaco) => {
             setRequirementsEditor(editor)
           }}
+          options={{
+            automaticLayout: true,
+          }}
         />
       </div>
-      <div>
-        {connected ? 'connected' : 'not connected'}
-      </div>
-      <Grid container direction="row-reverse" style={{ padding: 10 }}>
-        <Button variant="contained" color="primary" onClick={() => playProgram()}>play</Button>
+      <Grid container id="middle-container" justify='flex-end'>
+        <Grid item>
+          <Box p={1}>
+            <Button
+              variant="contained" color="default"
+              onClick={() => setLogText('')}
+            >
+              Clear log
+            </Button>
+            <Button
+              variant="contained" color="primary"
+              onClick={() => playProgram()}
+            >
+              play
+          </Button>
+          </Box>
+        </Grid>
       </Grid>
-      <pre key={logCount}>
+      <pre id="bottom-container" style={{ overflow: 'scroll', background: 'lightgray' }}>
         {logText}
       </pre>
     </div>
